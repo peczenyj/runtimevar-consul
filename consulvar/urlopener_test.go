69b68d1e2269435c6fc1b573750d0d8b55cc60d9
Tiago Peczenyj <tpeczenyj@weborama.com>
2026-05-25 11:58:45 +0200

    test: improve coverage for URLOpener client caching and pre-configured client

--- consulvar/urlopener_test.go
+++ consulvar/urlopener_test.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/hashicorp/consul/api"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gocloud.dev/runtimevar"
@@ -79,21 +80,30 @@ func TestURLOpener_WithFallbackDecoder(t *testing.T) {
 }
 
 func TestURLOpener_ClientCaching(t *testing.T) {
-	o := &consulvar.URLOpener{}
-	u, _ := url.Parse("consul://key1")
 	ctx := context.Background()
+	u, _ := url.Parse("consul://key1")
 
-	v1, err := o.OpenVariableURL(ctx, u)
-	require.NoError(t, err)
-	v1.Close()
+	t.Run("lazy client is cached", func(t *testing.T) {
+		o := &consulvar.URLOpener{}
+		v1, err := o.OpenVariableURL(ctx, u)
+		require.NoError(t, err)
+		v1.Close()
 
-	// Capture the first client (using a hacky way since it's private, or just
-	// by opening another and seeing if they share same state if I could.
-	// Actually, I can't easily see it from outside, but I can at least
-	// ensure the code path is hit multiple times.
-	v2, err := o.OpenVariableURL(ctx, u)
-	require.NoError(t, err)
-	v2.Close()
+		v2, err := o.OpenVariableURL(ctx, u)
+		require.NoError(t, err)
+		v2.Close()
+	})
+
+	t.Run("pre-configured client is used", func(t *testing.T) {
+		cfg := api.DefaultConfig()
+		client, err := api.NewClient(cfg)
+		require.NoError(t, err)
+
+		o := &consulvar.URLOpener{Client: client}
+		v1, err := o.OpenVariableURL(ctx, u)
+		require.NoError(t, err)
+		v1.Close()
+	})
 }
 
 // TestOpenVariableURL_SchemeRegistered checks that init() registered the
